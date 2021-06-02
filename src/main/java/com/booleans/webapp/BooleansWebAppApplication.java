4ddc60065422b7a0ec94f93f78c42eaded68e80e
AdityaS1426 <71394490+AdityaS1426@users.noreply.github.com>
2021-06-01 18:32:50 -0700

    Initial commit

---
+++ src/main/java/com/booleans/webapp/BooleansWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.booleans.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooleansWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooleansWebAppApplication.class, args);
+    }
+
+}
